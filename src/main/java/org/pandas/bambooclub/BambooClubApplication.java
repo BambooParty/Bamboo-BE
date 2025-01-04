ced8abe4333f98245e45e069ba7c0b1d99d640a2
minisun <minisundev@gmail.com>
2025-01-04 10:38:37 +0900

    chore: init commit

---
+++ src/main/java/org/pandas/bambooclub/BambooClubApplication.java
@@ -0,0 +1,13 @@
+package org.pandas.bambooclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BambooClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pandas/bambooclub/BambooClubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pandas.bambooclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
