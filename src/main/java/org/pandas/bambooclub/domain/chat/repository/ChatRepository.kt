f22d5831ff74ea19e61cddfb5e5e91b2b824c714
minisun <minisundev@gmail.com>
2025-01-04 19:37:19 +0900

    feat: add findAllByUserIdAndChatRoomId to ChatRepository

--- src/main/java/org/pandas/bambooclub/domain/chat/repository/ChatRepository.kt
+++ src/main/java/org/pandas/bambooclub/domain/chat/repository/ChatRepository.kt
@@ -5,4 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ChatRepository : MongoRepository<Chat, String>
+interface ChatRepository : MongoRepository<Chat, String> {
+    fun findAllByUserIdAndChatRoomId(
+        userId: String,
+        chatRoomId: String,
+    ): List<Chat>
+}
