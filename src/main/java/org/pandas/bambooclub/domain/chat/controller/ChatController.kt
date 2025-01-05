e9138105979065abff459b2ab37262b7851d0fa6
minisun <minisundev@gmail.com>
2025-01-05 20:19:07 +0900

    feat: changed to RequestParam

--- src/main/java/org/pandas/bambooclub/domain/chat/controller/ChatController.kt
+++ src/main/java/org/pandas/bambooclub/domain/chat/controller/ChatController.kt
@@ -3,14 +3,12 @@
 package org.pandas.bambooclub.domain.chat.controller
 
 import kotlinx.coroutines.runBlocking
-import org.pandas.bambooclub.domain.chat.dto.ChatRequest
 import org.pandas.bambooclub.domain.chat.dto.ChatResponse
 import org.pandas.bambooclub.domain.chat.service.ChatService
 import org.pandas.bambooclub.global.security.UserPrincipal
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -20,11 +18,11 @@ class ChatController(
 ) {
     @PostMapping("/chats")
     fun chat(
-        @Validated @RequestBody request: ChatRequest,
+        @RequestParam content: String,
         @AuthenticationPrincipal principal: UserPrincipal,
     ): ResponseEntity<ChatResponse> {
         return runBlocking {
-            ResponseEntity(chatService.chat(principal, request), HttpStatus.OK)
+            ResponseEntity(chatService.chat(principal, content), HttpStatus.OK)
         }
     }
 
